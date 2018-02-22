282862710454312b68f0a307fdf9fc39e30050bb
sunxfancy <sunxfancy@gmail.com>
2018-02-21 16:07:50 -0800

    first version

---
+++ conanfile.py
@@ -0,0 +1,37 @@
+from conans import ConanFile, CMake, tools
+
+class LLVMCodeRunnerConan(ConanFile):
+    name = "LLVMCodeRunner"
+    version = "0.1.0"
+    license = "MIT"
+    url = "https://github.com/sunxfancy/LLVMCodeRunner"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False", "gtest:shared=False"
+    generators = "cmake"
+    build_policy = "missing"
+    requires = 'gtest/1.8.0@lasote/stable', 'llvm/5.0.1@sunxfancy/stable'
+    exports = "*"
+    
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def imports(self):
+        self.copy(pattern="*.dll", dst="bin", src="bin")
+        self.copy(pattern="*.dylib", dst="bin", src="lib")
+        self.copy(pattern="*.so", dst="bin", src="lib")
+        self.copy(pattern="*", dst="bin", src="bin")
+        
+    def package(self):
+        self.copy("*.h", dst="include", src="include")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("LICENSE", dst=".", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = [self.name]
