c5dbe8ab8ee6a5949822ff0ecc05c41af141e37f
sunxfancy <sunxfancy@gmail.com>
2018-02-21 17:33:38 -0800

    add loop pass for test

--- RunnerPass/Pass.cpp
+++ RunnerPass/Pass.cpp
@@ -1,4 +1,5 @@
 #include "llvm/Pass.h"
+#include "llvm/Analysis/LoopPass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
 
@@ -6,18 +7,36 @@ using namespace llvm;
 
 namespace {
 struct Hello : public FunctionPass {
-  static char ID;
-  Hello() : FunctionPass(ID) {}
+    static char ID;
+    Hello() : FunctionPass(ID) {}
 
-  bool runOnFunction(Function &F) override {
-    errs() << "Hello: ";
-    errs().write_escaped(F.getName()) << '\n';
-    return false;
-  }
+    bool runOnFunction(Function &F) override {
+        errs() << "Hello: ";
+        errs().write_escaped(F.getName()) << '\n';
+        return false;
+    }
 }; // end of struct Hello
-}  // end of anonymous namespace
+} // end of anonymous namespace
 
 char Hello::ID = 0;
 static RegisterPass<Hello> X("hello", "Hello World Pass",
                              false /* Only looks at CFG */,
                              false /* Analysis Pass */);
+
+namespace {
+struct LoopDector : public LoopPass {
+    static char ID;
+    LoopDector() : LoopPass(ID) {}
+
+    bool runOnLoop(Loop *L, LPPassManager &LPM) override {
+        errs() << "LoopDector: ";
+        errs().write_escaped(L->) << '\n';
+        return false;
+    }
+}; // end of struct LoopDector
+} // end of anonymous namespace
+
+char LoopDector::ID = 1;
+static RegisterPass<LoopDector> Y("loop-detector", "LoopDector Pass",
+                                  false /* Only looks at CFG */,
+                                  false /* Analysis Pass */);
